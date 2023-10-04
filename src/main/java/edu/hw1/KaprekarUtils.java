cbb97b6dccc10881da5f9b226845d5e7486e25ad
BITniki <shakarimovdima@ya.ru>
2023-10-04 22:53:31 +0300

    task 6: fix on edge cases behavior

--- src/main/java/edu/hw1/KaprekarUtils.java
+++ src/main/java/edu/hw1/KaprekarUtils.java
@@ -13,7 +13,7 @@ public final class KaprekarUtils {
 
     /**
      * Sort given number digits in asc or desc order.
-     * @param num to sort
+     * @param num to sort. Only non-negative numbers allowed
      * @param desc sorting order
      * @return number with sorted digits
      */
@@ -41,11 +41,15 @@ public final class KaprekarUtils {
 
     /**
      * Applying the Kaprekar function to a number.
-     * If output number less than 1000 function will add zeros to the number right.
-     * @param num to apply
+     * If the output number is less than 1000 function will add zeros to the number right.
+     * @param num to apply. Must have four digits
      * @return the Kaprekar function output
      */
     public static int kaprekarFunc(int num) {
+        if (num <= 0 || CountNumberUtils.countDigits(num) != ALLOWED_DIGIT_COUNT) {
+            throw new IllegalArgumentException("Can't apply function kaprekar to %d".formatted(num));
+        }
+
         int newNum =  sortDigitsInNumber(num, true) - sortDigitsInNumber(num, false);
 
         int digitCountDiff = ALLOWED_DIGIT_COUNT - CountNumberUtils.countDigits(newNum);
@@ -58,12 +62,12 @@ public final class KaprekarUtils {
 
     /**
      * Return the number of steps that need to make Kaprekar constant from given number.
-     * @param num to iterate
+     * @param num to iterate. Must be positive
      * @return count of iteration
      * @throws IllegalArgumentException if number cannot become a Kaprekar constant
      */
     public static int countK(int num) {
-        if (CountNumberUtils.countDigits(num) != ALLOWED_DIGIT_COUNT) {
+        if (num <= 0 || CountNumberUtils.countDigits(num) != ALLOWED_DIGIT_COUNT) {
             throw new IllegalArgumentException("the number %d cannot become a Kaprekar constant".formatted(num));
         }
 
