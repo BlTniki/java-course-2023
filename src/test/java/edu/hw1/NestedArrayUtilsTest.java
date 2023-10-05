d6b8e0f3fa2f4be1e546f765ba5a65cdd1476c44
BITniki <shakarimovdima@ya.ru>
2023-10-05 22:09:24 +0300

    task 3: remove unused import

--- src/test/java/edu/hw1/NestedArrayUtilsTest.java
+++ src/test/java/edu/hw1/NestedArrayUtilsTest.java
@@ -1,12 +1,10 @@
 package edu.hw1;
 
+import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
-import java.util.List;
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class NestedArrayUtilsTest {
 
