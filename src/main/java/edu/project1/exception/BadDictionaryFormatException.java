14d2aef2e2a30b3db9e539b3f6509b4c9b189b56
BITniki <shakarimovdima@ya.ru>
2023-10-07 23:26:06 +0300

    project 1: init

---
+++ src/main/java/edu/project1/exception/BadDictionaryFormatException.java
@@ -0,0 +1,7 @@
+package edu.project1.exception;
+
+public class BadDictionaryFormatException extends Exception {
+    public BadDictionaryFormatException(String message) {
+        super(message);
+    }
+}
