e9cf4378a3fc0b74a9b7f0002078465a02522311
BITniki <shakarimovdima@ya.ru>
2023-10-07 23:20:50 +0300

    project 1: add word validation

--- src/main/java/edu/project1/DictionaryFromResources.java
+++ src/main/java/edu/project1/DictionaryFromResources.java
@@ -1,12 +1,13 @@
 package edu.project1;
 
-import org.jetbrains.annotations.NotNull;
+import edu.project1.exception.BadDictionaryFormatException;
 import java.io.FileNotFoundException;
 import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 import java.util.Scanner;
+import org.jetbrains.annotations.NotNull;
 
 /**
  * This class stores dictionary for game.
@@ -15,24 +16,38 @@ import java.util.Scanner;
  */
 public class DictionaryFromResources implements Dictionary {
     /**
+     * Regex that match with string were several words.
+     */
+    @SuppressWarnings({"RegExpSimplifiable"})
+    private final static String INCORRECT_WORD_REGEX = "^.*[\\s].*$";
+
+    /**
      * Dictionary where stored all words from file.
      */
     private final List<String> dict;
 
-    public DictionaryFromResources(String dictFileName) throws FileNotFoundException {
-        this.dict = readDictFromFile(dictFileName);
+    public DictionaryFromResources(String dictFileName)
+        throws FileNotFoundException, BadDictionaryFormatException {
+        ClassLoader classloader = Thread.currentThread().getContextClassLoader();
+        this.dict = readDictFromFile(classloader, dictFileName);
+    }
+
+    @Override
+    public @NotNull String randomWord() {
+        Random rand = new Random();
+        return dict.get(rand.nextInt(dict.size()));
     }
 
     /**
      * Reading dictionary from file in resources dir Util.
+     * @param classLoader context loader were that in resources
      * @param fileName where dict is stored
-     * @return dictionary of words for game
+     * @return dictionary of words for game. On each line should be one word.
      * @throws FileNotFoundException if a file with this name is not found
      */
-    public static List<String> readDictFromFile(String fileName) throws FileNotFoundException {
+    public static List<String> readDictFromFile(ClassLoader classLoader, String fileName)
+        throws FileNotFoundException, BadDictionaryFormatException {
         List<String> strings = new ArrayList<>();
-
-        ClassLoader classLoader = Main.class.getClassLoader();
 
         InputStream inputStream = classLoader.getResourceAsStream(fileName);
 
@@ -40,7 +55,15 @@ public class DictionaryFromResources implements Dictionary {
             Scanner scanner = new Scanner(inputStream);
 
             while (scanner.hasNextLine()) {
-                strings.add(scanner.nextLine());
+                final String word = scanner.nextLine();
+
+                if (word.isEmpty() || word.matches(INCORRECT_WORD_REGEX)) {
+                    throw new BadDictionaryFormatException(
+                        "File contain bad word line like -> '%s'".formatted(word)
+                    );
+                }
+
+                strings.add(word);
             }
 
             scanner.close();
@@ -49,10 +72,5 @@ public class DictionaryFromResources implements Dictionary {
         }
 
         return strings;
-    }
-    @Override
-    public @NotNull String randomWord() {
-        Random rand = new Random();
-        return dict.get(rand.nextInt(dict.size()));
     }
 }
