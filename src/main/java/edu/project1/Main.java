be6826e53b0a45638e631ce18d9ba7ce0bb171fd
BITniki <shakarimovdima@ya.ru>
2023-10-08 14:01:29 +0300

    project 1: implement game configuration and game start

--- src/main/java/edu/project1/Main.java
+++ src/main/java/edu/project1/Main.java
@@ -1,25 +1,30 @@
 package edu.project1;
 
+import edu.project1.exception.BadDictionaryFormatException;
+import java.io.FileNotFoundException;
 import java.util.Scanner;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 public final class Main {
     private final static Logger LOGGER = LogManager.getLogger();
+    private static final String DICT_FILE_NAME = "dict.txt";
+    private static final int MAX_ATTEMPTS = 5;
 
     private Main() {
     }
 
-    public static void main(final String[] args) {
-//        Scanner scanner = new Scanner(System.in);
-//
-//        System.out.println("Enter some text (Ctrl+D or Ctrl+Z to exit):");
-//
-//        while (scanner.hasNextLine()) {
-//            String line = scanner.nextLine();
-//            System.out.println("You entered: " + line);
-//        }
-//
-//        System.out.println("End of input. Exiting...");
+    public static void main(final String[] args) throws FileNotFoundException, BadDictionaryFormatException {
+        final Dictionary dictionary = new DictionaryFromResources(DICT_FILE_NAME);
+        final Scanner scanner = new Scanner(System.in);
+
+        LOGGER.info("Starting new game!");
+        LOGGER.info("max attempts: " + MAX_ATTEMPTS);
+        LOGGER.info("dictionary file: " + DICT_FILE_NAME);
+        LOGGER.info("Type any character to guess the word! Guess is not case sensitive.");
+        LOGGER.info("You can give up by pressing Ctrl+D...\n");
+
+        final ConsoleHangman hangman = new ConsoleHangman(LOGGER, scanner, dictionary, MAX_ATTEMPTS);
+        hangman.run();
     }
 }
