db137104078ec4fdd63ddc1f194757aaf877ac69
BITniki <shakarimovdima@ya.ru>
2023-10-08 10:26:44 +0300

    project 1: Cover Dictionary with test

--- src/test/java/edu/project1/DictionaryFromResourcesTest.java
+++ src/test/java/edu/project1/DictionaryFromResourcesTest.java
@@ -8,6 +8,7 @@ import org.apache.logging.log4j.Logger;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class DictionaryFromResourcesTest {
 
@@ -31,6 +32,65 @@ class DictionaryFromResourcesTest {
     }
 
     @Test
-    void readDictFromFile() {
+    @DisplayName("Загрузка словаря: good")
+    void readDictFromFile_good() throws FileNotFoundException, BadDictionaryFormatException {
+        // given
+        String fileName = "good_dict.txt";
+        List<String> expectedList = List.of("foo", "bar", "lolkek", "cheburek");
+        ClassLoader classloader = Thread.currentThread().getContextClassLoader();
+
+        // when
+        List<String> returnedList = DictionaryFromResources.readDictFromFile(classloader, fileName);
+
+        // then
+        assertThat(returnedList).isEqualTo(expectedList);
+    }
+
+    @Test
+    @DisplayName("Загрузка словаря: файл не найден")
+    void readDictFromFile_bad_non_exist() {
+        // given
+        String fileName = "bad_dict_non_exist.txt";
+        ClassLoader classloader = Thread.currentThread().getContextClassLoader();
+
+        assertThatThrownBy(() -> DictionaryFromResources.readDictFromFile(classloader, fileName))
+            .isInstanceOf(FileNotFoundException.class)
+            .hasMessage("File '%s' not found".formatted(fileName));
+    }
+
+    @Test
+    @DisplayName("Загрузка словаря: пустой словарь")
+    void readDictFromFile_bad_empty() {
+        // given
+        String fileName = "bad_dict_empty.txt";
+        ClassLoader classloader = Thread.currentThread().getContextClassLoader();
+
+        assertThatThrownBy(() -> DictionaryFromResources.readDictFromFile(classloader, fileName))
+            .isInstanceOf(BadDictionaryFormatException.class)
+            .hasMessage("File '%s' is empty".formatted(fileName));
+    }
+
+    @Test
+    @DisplayName("Загрузка словаря: пустая строка")
+    void readDictFromFile_bad_empty_string() {
+        // given
+        String fileName = "bad_dict_empty_string.txt";
+        ClassLoader classloader = Thread.currentThread().getContextClassLoader();
+
+        assertThatThrownBy(() -> DictionaryFromResources.readDictFromFile(classloader, fileName))
+            .isInstanceOf(BadDictionaryFormatException.class)
+            .hasMessage("File contain bad word line like -> ''");
+    }
+
+    @Test
+    @DisplayName("Загрузка словаря: плохая строка")
+    void readDictFromFile_bad_string() {
+        // given
+        String fileName = "bad_dict_string.txt";
+        ClassLoader classloader = Thread.currentThread().getContextClassLoader();
+
+        assertThatThrownBy(() -> DictionaryFromResources.readDictFromFile(classloader, fileName))
+            .isInstanceOf(BadDictionaryFormatException.class)
+            .hasMessage("File contain bad word line like -> 'kek kek'");
     }
 }
