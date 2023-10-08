a293de97e565ada6e82550591d304ecb95d1c49c
BITniki <shakarimovdima@ya.ru>
2023-10-08 14:00:38 +0300

    project 1: implement internal guess logic

--- src/main/java/edu/project1/Session.java
+++ src/main/java/edu/project1/Session.java
@@ -1,14 +1,72 @@
 package edu.project1;
 
+import java.util.Set;
+import java.util.stream.Collectors;
 import org.jetbrains.annotations.NotNull;
 
 class Session {
     private final String answer;
-    private final char[] userAnswer;
     private final int maxAttempts;
+    private final Set<Character> unguessedCharacters;
     private int attempts;
+    private boolean dead;
 
-    @NotNull GuessResult guess(char guess);
+    Session(String answer, int maxAttempts) {
+        this.answer = answer;
+        this.maxAttempts = maxAttempts;
+        this.attempts = 0;
+        this.dead = false;
 
-    @NotNull GuessResult giveUp();
+        this.unguessedCharacters = answer.toLowerCase().chars()
+            .mapToObj(c -> (char) c)
+            .collect(Collectors.toSet());
+    }
+
+    private String provideUserAnswer() {
+        String userAnswer = answer;
+
+        for (char c : unguessedCharacters) {
+            userAnswer = userAnswer.replace(Character.toLowerCase(c), '*');
+            userAnswer = userAnswer.replace(Character.toUpperCase(c), '*');
+        }
+
+        return userAnswer;
+    }
+
+    /**
+     * Checking player guess.
+     * @param guess player guess. Guess is case-insensitive
+     * @return guess result
+     */
+    @NotNull GuessResult guess(char guess) {
+        if (unguessedCharacters.remove(guess)) {
+            if (unguessedCharacters.isEmpty()) {
+                dead = true;
+                return new GuessResult.Win(provideUserAnswer(), attempts, maxAttempts);
+            } else {
+                return new GuessResult.SuccessfulGuess(provideUserAnswer(), attempts, maxAttempts);
+            }
+        } else {
+            attempts += 1;
+            if (attempts >= maxAttempts) {
+                dead = true;
+                return new GuessResult.Defeat(provideUserAnswer(), attempts, maxAttempts);
+            } else {
+                return new GuessResult.FailedGuess(provideUserAnswer(), attempts, maxAttempts);
+            }
+        }
+    }
+
+    /**
+     * Killing session.
+     * @return give up message
+     */
+    @NotNull GuessResult giveUp() {
+        dead = true;
+        return new GuessResult.GiveUp(provideUserAnswer(), attempts, maxAttempts);
+    }
+
+    boolean isNotDead() {
+        return !this.dead;
+    }
 }
