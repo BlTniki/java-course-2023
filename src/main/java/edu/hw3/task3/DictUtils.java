f87e1b5bd8ed7de99f0a6d175603cdc61d957e42
BITniki <shakarimovdima@ya.ru>
2023-10-23 12:12:13 +0300

    task3: refactor working with types

--- src/main/java/edu/hw3/task3/DictUtils.java
+++ src/main/java/edu/hw3/task3/DictUtils.java
@@ -14,8 +14,8 @@ public final class DictUtils {
      * @param list elems to count
      * @return dict with elems frequency
      */
-    public static Map<?, Integer> freqDict(final @NotNull List<Object> list) {
-        final HashMap<Object, Integer> dict = new HashMap<>();
+    public static <T> Map<T, Integer> freqDict(final @NotNull List<T> list) {
+        final HashMap<T, Integer> dict = new HashMap<>();
 
         for (var el : list) {
             dict.put(el, dict.getOrDefault(el, 0) + 1);
