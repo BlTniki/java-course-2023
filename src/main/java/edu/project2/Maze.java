7f6f3cdb8634e5a70e4aad0b242cb82d5b522edc
BITniki <shakarimovdima@ya.ru>
2023-10-26 13:37:01 +0300

    implement Maze

--- src/main/java/edu/project2/Maze.java
+++ src/main/java/edu/project2/Maze.java
@@ -1,7 +1,19 @@
 package edu.project2;
 
 public final class Maze {
-//    private final int height;
-//    private final int width;
-//    private final Cell[][] grid;
+    private final int height;
+    private final int width;
+    private final Cell[][] grid;
+
+    public Maze(int height, int width) {
+        this.height = height;
+        this.width = width;
+        this.grid = new Cell[height][width];
+    }
+
+    public Maze(int height, int width, Cell[][] grid) {
+        this.height = height;
+        this.width = width;
+        this.grid = grid;
+    }
 }
---
+++ src/test/java/edu/project2/MazeTest.java
@@ -0,0 +1,12 @@
+package edu.project2;
+
+import org.junit.jupiter.api.Test;
+
+class MazeTest {
+    @Test
+    void del_me() {
+//        var m = new Maze(10,10);
+//        var t = m.grid[2][2];
+//        var c = 123;
+    }
+}
