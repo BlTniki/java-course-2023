d2ccfc79c61e4255922455a090de6fd11e5c217d
BITniki <shakarimovdima@ya.ru>
2023-10-26 13:36:41 +0300

    init RendererImpl

---
+++ src/main/java/edu/project2/renderer/RendererImpl.java
@@ -0,0 +1,18 @@
+package edu.project2.renderer;
+
+import edu.project2.Coordinate;
+import edu.project2.Maze;
+import java.util.List;
+
+public class RendererImpl implements Renderer {
+
+    @Override
+    public String render(Maze maze) {
+        return null;
+    }
+
+    @Override
+    public String render(Maze maze, List<Coordinate> path) {
+        return null;
+    }
+}
