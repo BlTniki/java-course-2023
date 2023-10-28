aeaedc4ebb96bf4a23365d6cfbfdd163cc80a8f4
BITniki <shakarimovdima@ya.ru>
2023-10-28 13:21:51 +0300

    add changeType() method

--- src/main/java/edu/project2/Cell.java
+++ src/main/java/edu/project2/Cell.java
@@ -1,8 +1,14 @@
 package edu.project2;
 
+import org.jetbrains.annotations.NotNull;
+
 public record Cell(int row, int col, Type type) {
     public enum Type {
         WALL,
         PASSAGE
+    }
+
+    public @NotNull Cell changeType(Type type) {
+        return new Cell(row, col, type);
     }
 }
