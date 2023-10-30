a3b74479589beea656a2c3f31c5af2504832ea73
BITniki <shakarimovdima@ya.ru>
2023-10-30 11:39:33 +0300

    implement GeneratorManager

---
+++ src/main/java/edu/project2/generator/GeneratorManager.java
@@ -0,0 +1,22 @@
+package edu.project2.generator;
+
+import java.util.Random;
+
+public class GeneratorManager {
+    private Random random;
+
+    public GeneratorManager(Random random) {
+        this.random = random;
+    }
+
+    public Generator getInstanceOf(GenerationType type) {
+        switch (type) {
+            case RecursiveDivision -> {
+                return new RecursiveDivisionGenerator(random);
+            }
+            default -> {
+                return new RecursiveDivisionGenerator(random);
+            }
+        }
+    }
+}
