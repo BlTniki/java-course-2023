8a2e2327be42df90f0e1ea50f050ceb15f5eacf9
BITniki <shakarimovdima@ya.ru>
2023-11-01 23:08:06 +0300

    task2: cover with tests

---
+++ src/test/java/edu/hw5/task2/FridayThe13thUtilsTest.java
@@ -0,0 +1,35 @@
+package edu.hw5.task2;
+
+import java.time.LocalDate;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+class FridayThe13thUtilsTest {
+    @Test
+    void findFridaysThe13th_shouldReturnCorrectDatesForYear2023() {
+        // given
+        int year = 2023;
+        List<LocalDate> expected = List.of(
+            LocalDate.of(2023, 1, 13),
+            LocalDate.of(2023, 10, 13)
+        );
+
+        // when
+        List<LocalDate> actual = FridayThe13thUtils.findFridaysThe13th(year);
+
+        // then
+        assertThat(actual).containsExactlyInAnyOrderElementsOf(expected);
+    }
+
+    @Test
+    void findNextFridayThe13th_shouldReturnCorrectDateAfterGivenDate() {
+        // given
+        LocalDate inputDate = LocalDate.of(2023, 11, 1);
+        LocalDate expected = LocalDate.of(2024, 9, 13);
+
+        LocalDate actual = FridayThe13thUtils.findNextFridayThe13th(inputDate);
+
+        assertThat(actual).isEqualTo(expected);
+    }
+}
