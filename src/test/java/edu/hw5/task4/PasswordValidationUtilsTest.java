81dc852e4324ef2efe801eb20534e8fef224b9a5
BITniki <shakarimovdima@ya.ru>
2023-11-02 20:45:22 +0300

    task4: cover with tests

---
+++ src/test/java/edu/hw5/task4/PasswordValidationUtilsTest.java
@@ -0,0 +1,29 @@
+package edu.hw5.task4;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import static org.assertj.core.api.Assertions.assertThat;
+
+class PasswordValidationUtilsTest {
+    static Arguments[] passwords() {
+        return new Arguments[] {
+            Arguments.of("$", true),
+            Arguments.of("321*", true),
+            Arguments.of("&daw", true),
+            Arguments.of("3213%dwad", true),
+            Arguments.of("3$213%dwad", true),
+            Arguments.of("", false),
+            Arguments.of("123", false),
+            Arguments.of("adawd", false),
+            Arguments.of("  ", false),
+            Arguments.of(null, false)
+        };
+    }
+    @ParameterizedTest
+    @MethodSource("passwords")
+    void isPasswordValid(String password, boolean expected) {
+        assertThat(PasswordValidationUtils.isPasswordValid(password))
+            .isEqualTo(expected);
+    }
+}
