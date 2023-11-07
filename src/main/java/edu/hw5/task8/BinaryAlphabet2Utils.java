66d0c0e5e00030a4c13f6c834b025988b2f32e19
BITniki <shakarimovdima@ya.ru>
2023-11-07 15:40:55 +0300

    task 8: implement solution

---
+++ src/main/java/edu/hw5/task8/BinaryAlphabet2Utils.java
@@ -0,0 +1,49 @@
+package edu.hw5.task8;
+
+import org.jetbrains.annotations.NotNull;
+import java.util.regex.Pattern;
+
+/**
+ * If BinaryAlphabetUtils was so good why there no BinaryAlphabetUtils2....
+ */
+public final class BinaryAlphabet2Utils {
+
+    private BinaryAlphabet2Utils() {
+    }
+
+    /*
+    task 8.1
+     */
+    public static boolean isWordHasOddLength(@NotNull String word) {
+        return Pattern.compile("^([01][01])*[01]$")
+            .matcher(word)
+            .matches();
+    }
+
+    /*
+    task 8.2
+     */
+    public static boolean zeroOrOne(@NotNull String word) {
+        return Pattern.compile("(^0([01][01])*$)|(^1[01]([01][01])*$)")
+            .matcher(word)
+            .matches();
+    }
+
+    /*
+    task 8.4
+     */
+    public static boolean notContain11or111(@NotNull String word) {
+        return Pattern.compile("^(?!1{2,3}$)[01]*$")
+            .matcher(word)
+            .matches();
+    }
+
+    /*
+    task 8.5
+     */
+    public static boolean has1AtOddIndexes(@NotNull String word) {
+        return Pattern.compile("^(1[01])*1?")
+            .matcher(word)
+            .matches();
+    }
+}
