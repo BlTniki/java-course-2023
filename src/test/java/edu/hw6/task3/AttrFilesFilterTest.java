1e21712d61ead788675ba6d8b4bad8f14fd8a5e7
BITniki <shakarimovdima@ya.ru>
2023-11-17 16:58:08 +0300

    task 3: cover with tests

---
+++ src/test/java/edu/hw6/task3/AttrFilesFilterTest.java
@@ -0,0 +1,29 @@
+package edu.hw6.task3;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+import java.util.stream.StreamSupport;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+class AttrFilesFilterTest {
+
+    @Test
+    @DisplayName("Проверим что файлы фильтруются по атрибуту")
+    void check() {
+        AttrFilesFilter filter = new AttrFilesFilter(List.of(
+            new AttrFilesFilter.Attr("dos:readonly", true)
+        ));
+
+        try (var ds = Files.newDirectoryStream(Path.of("./src/test/resources/files/attr"), filter)) {
+            List<Path> list = StreamSupport.stream(ds.spliterator(), false).toList();
+            assertThat(list)
+                .containsOnly(Path.of("./src/test/resources/files/attr/nonwritable"));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
---
+++ src/test/java/edu/hw6/task3/FilesFilterTest.java
@@ -0,0 +1,30 @@
+package edu.hw6.task3;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+import java.util.stream.StreamSupport;
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+class FilesFilterTest {
+
+    @Test
+    void check() {
+        FilesFilter filter = new AttrFilesFilter(List.of(
+            new AttrFilesFilter.Attr("dos:readonly", true)
+            )).and(new SizeFilesFilter(16, true))
+            .and(new RegexFilesFilter("^foo.*$"))
+            .and(new ExtensionFilesFilter("txt"))
+            .and(new MagicBytesFilesFilter(new byte[] {(byte) 0x89, 0x50, 0x4e, 0x47}));
+
+        try (var ds = Files.newDirectoryStream(Path.of("./src/test/resources/files/chain"), filter)) {
+            List<Path> list = StreamSupport.stream(ds.spliterator(), false).toList();
+            assertThat(list)
+                .containsOnly(Path.of("./src/test/resources/files/chain/foo.txt"));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
