dc3e37cb8cbc930a8e14a0d324fe8f010c1c5757
BITniki <shakarimovdima@ya.ru>
2023-11-19 12:13:57 +0300

    init Main class

--- src/main/java/edu/project3/Main.java
+++ src/main/java/edu/project3/Main.java
@@ -1,37 +1,10 @@
 package edu.project3;
 
-import java.io.IOException;
-import java.nio.file.FileSystem;
-import java.nio.file.FileSystems;
-import java.nio.file.FileVisitResult;
-import java.nio.file.FileVisitor;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.PathMatcher;
-import java.nio.file.SimpleFileVisitor;
-import java.nio.file.attribute.BasicFileAttributes;
-import java.util.ArrayList;
-import java.util.List;
-
-public class Main {
-    static List<String> searchWithWc(Path rootDir, String pattern) throws IOException {
-        List<String> matchesList = new ArrayList<>();
-        FileSystem fs = FileSystems.getDefault();
-        PathMatcher matcher = fs.getPathMatcher(pattern);
-        FileVisitor<Path> matcherVisitor = new SimpleFileVisitor<Path>() {
-            @Override
-            public FileVisitResult visitFile(Path file, BasicFileAttributes attribs) {
-                Path name = file.getFileName();
-                if (matcher.matches(name)) {
-                    matchesList.add(name.toString());
-                }
-                return FileVisitResult.CONTINUE;
-            }
-        };
-        Files.walkFileTree(rootDir, matcherVisitor);
-        return matchesList;
+public final class Main {
+    private Main() {
     }
-    public static void main(String[] args) throws IOException {
+
+    public static void main(String[] args) {
 
     }
 }
