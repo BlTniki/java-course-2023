eb8d18fe8a386906229b263cf880fc7e69397fdd
BITniki <shakarimovdima@ya.ru>
2023-11-30 09:52:55 +0300

    add disk variation

--- src/main/java/edu/project4/transformer/variation/VariationType.java
+++ src/main/java/edu/project4/transformer/variation/VariationType.java
@@ -7,8 +7,16 @@ public enum VariationType {
     LINEAR(point -> new Point(point.x(), point.y())),
     SINUSOIDAL(point -> new Point(Math.sin(point.x()), Math.sin(point.y()))),
     SPHERICAL(point -> {
-        final double r = point.x() * point.x() + point.y() * point.y();
-        return new Point(point.x() / r, point.y() / r);
+        final double r2 = point.x() * point.x() + point.y() * point.y();
+        return new Point(point.x() / r2, point.y() / r2);
+    }),
+    DISK(point -> {
+        final double r = Math.sqrt(point.x() * point.x() + point.y() * point.y());
+        final double theta = Math.atan(point.x() / point.y());
+        return new Point(
+            (theta / Math.PI) * Math.sin(Math.PI * r),
+            (theta / Math.PI) * Math.cos(Math.PI * r)
+        );
     });
 
     public final Variation vari;
