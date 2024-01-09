6445cc1db2f76c583a7d7c0e9d95770a3ad9e2e0
BITniki <shakarimovdima@ya.ru>
2024-01-09 10:20:04 +0300

    mandelbrot: fix codestyle

--- src/main/java/edu/project4/mandelbrot/Main.java
+++ src/main/java/edu/project4/mandelbrot/Main.java
@@ -5,13 +5,16 @@ import edu.project4.histogram.HistoPoint;
 import javax.swing.SwingUtilities;
 
 public final class Main {
+
+    public static final int CONST = 1000;
+
     private Main() {
     }
 
     public static void main(String[] args) {
-        int xRes = 1000;
-        int yRes = 1000;
-        int threshold = 1000;
+        int xRes = CONST;
+        int yRes = CONST;
+        int threshold = CONST;
 
         Mandelbrot mandelbrot = new Mandelbrot(
             xRes,
